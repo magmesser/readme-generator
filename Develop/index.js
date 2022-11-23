7d1e55d6464d5888d36536fd7f39f23e976a90ce
magmesser <magmesser@gmail.com>
2022-11-23 14:54:01 -0500

    worked on questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Title: title of the project
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    // Description: description of the project 
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    // Table of Contents: table of contents of ReadMe with links to sections of ReadMe file
+    {
+        type: 'input',
+        message: 'How to use it?',
+        name: 'usage',
+    },
+    // Installation: installation instructions  
+    {
+        type: 'input',
+        message: 'Your Name',
+        name: 'author',
+    },
+    {
+        type: 'input',
+        message: 'Your Email',
+        name: 'email',
+    },
+    // Usage: usage information 
+    {
+        type: 'input',
+        message: 'GitHub Account',
+        name: 'github',
+    },
+    // License: license information 
+    {
+        type: 'list',
+        message: 'License',
+        name: 'license',
+        choices: ['MIT', 'ISC', 'MPL', 'GPL', 'AGPL']
+    },
+    // Contributing: contribution guidelines
+    {
+        type: ,
+        message: ,
+        name: ,
+    },
+    // Tests: test instructions 
+    {
+        type: ,
+        message: ,
+        name: ,
+    },
+    // Questions: GitHub username and profile link; email address with instructions on how to reach me
+    {
+        type: ,
+        message: ,
+        name: ,
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,9 +11,51 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
+/* GitHub Contents -- 
+Title: title of the project 
+Description: description of the project 
+Table of Contents: table of contents of ReadMe with links to sections of ReadMe file
+Installation: installation instructions  
+Usage: usage information 
+License: license information 
+Contributing: contribution guidelines
+Tests: test instructions 
+Questions: GitHub username and profile link; email address with instructions on how to reach me */
+
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description} 
+
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contribution 
+  ${data.contribution}
+
+  ## Tests
+  ${data.testing}
+
+  ## License
+  ${data.license}
+
+  ## Questions
+  github.com/${data.github}
+  ${data.email}
 `;
 }
 
